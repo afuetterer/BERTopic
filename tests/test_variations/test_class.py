c51a9c98fce57b1d9428b5ff8a4896dc7657c969
Heinz-Alexander Fuetterer <fuetterh@posteo.de>
2024-07-12 15:15:53 +0200

    test

--- tests/test_variations/test_class.py
+++ tests/test_variations/test_class.py
@@ -6,15 +6,20 @@ data = fetch_20newsgroups(subset="all", remove=("headers", "footers", "quotes"))
 classes = [data["target_names"][i] for i in data["target"]][:1000]
 
 
+sorted
+
+
 @pytest.mark.parametrize(
     "model",
-    [
-        ("kmeans_pca_topic_model"),
-        ("custom_topic_model"),
-        ("merged_topic_model"),
-        ("reduced_topic_model"),
-        ("online_topic_model"),
-    ],
+    sorted(
+        [
+            "kmeans_pca_topic_model",
+            "custom_topic_model",
+            "merged_topic_model",
+            "reduced_topic_model",
+            "online_topic_model",
+        ]
+    ),
 )
 def test_class(model, documents, request):
     topic_model = copy.deepcopy(request.getfixturevalue(model))
